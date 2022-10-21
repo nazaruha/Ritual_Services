299892038b96c85b7552cd73b48b2f1548a3cd8d
Nazar <fedunnazar20122001@gmail.com>
2022-10-21 18:27:30 +0300

    Connect class User

--- Context/MyDataContext.cs
+++ Context/MyDataContext.cs
@@ -4,15 +4,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Context.Entities;
 
 namespace Context
 {
-    internal class MyDataContext : DbContext // inherites this class to work with DB
+    public class MyDataContext : DbContext // inherites this class to work with DB
     {
         public MyDataContext()
         {
             this.Database.Migrate(); // automatically creates migrations
         }
+
+        DbSet<User> Users { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
