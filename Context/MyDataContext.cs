c4a03608993654cf9840dda019217aad16f74079
Nazar <fedunnazar20122001@gmail.com>
2022-10-21 18:11:49 +0300

    add DbContext Configuring

--- Context/MyDataContext.cs
+++ Context/MyDataContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,16 @@ using System.Threading.Tasks;
 
 namespace Context
 {
-    internal class MyDataContext
+    internal class MyDataContext : DbContext // inherites this class to work with DB
     {
+        public MyDataContext()
+        {
+            this.Database.Migrate(); // automatically creates migrations
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Integrated Security=True; Initial Catalog=RitualServices");
+        }
     }
 }
