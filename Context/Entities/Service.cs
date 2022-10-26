9c7c94432e98bdae4af5e54fce5993b34ffcd0bc
Nazar <fedunnazar20122001@gmail.com>
2022-10-26 20:05:12 +0300

    add entity for services' names

---
+++ Context/Entities/Service.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Context.Entities
+{
+    [Table("tblServices")]
+    public class Service
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required, StringLength(50)]
+        public string Name { get; set; }    
+    }
+}
