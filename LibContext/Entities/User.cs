604dfae71bf66bc87b97dfd941d0995a781bd7ea
Nazar <fedunnazar20122001@gmail.com>
2022-10-31 14:51:00 +0200

    create new libClass with correct migrations

---
+++ LibContext/Entities/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibContext.Entities
+{
+    [Table("tblUsers")]
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required, StringLength(50)]
+        public string Login { get; set; }
+        [Required, StringLength(100)]
+        public string Password { get; set; }
+        [Required, StringLength(100)]
+        public string Email { get; set; }
+    }
+}
