b4a344989e18e8d5043b40b6f46e513c94fe7c83
Grig_os85@student.itstep.org <ivanhrygorutsa@gmail.com>
2022-10-31 13:55:16 +0100

    v0.2

---
+++ UserApplication/Church-Web.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UserApplication
+{
+    /// <summary>
+    /// Логика взаимодействия для Church_Web.xaml
+    /// </summary>
+    public partial class Church_Web : Window
+    {
+        public Church_Web()
+        {
+            InitializeComponent();
+        }
+        private void Button_Info_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Order_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
