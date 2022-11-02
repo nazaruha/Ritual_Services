6b2ee77b4dfe959fd1ba02d0038b619167c9a0d9
Nazar <fedunnazar20122001@gmail.com>
2022-11-02 11:37:24 +0200

    update migrations for photo field

--- LibContext/Migrations/20221031124051_add tblServices.cs
+++ LibContext/Migrations/20221031124051_add tblServices.cs
@@ -19,6 +19,7 @@ namespace LibContext.Migrations
                     Price = table.Column<int>(type: "int", nullable: false),
                     DescriptionEng = table.Column<string>(type: "nvarchar(2000)", maxLength: 2000, nullable: false),
                     DescriptionUA = table.Column<string>(type: "nvarchar(2000)", maxLength: 2000, nullable: false),
+                    Photo = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
                     ServiceTypeId = table.Column<int>(type: "int", nullable: false)
                 },
                 constraints: table =>
--- LibContext/Migrations/MyDataContextModelSnapshot.cs
+++ LibContext/Migrations/MyDataContextModelSnapshot.cs
@@ -29,6 +29,11 @@ namespace LibContext.Migrations
 
                     SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"), 1L, 1);
 
+                    b.Property<string>("Photo")
+                        .IsRequired()
+                        .HasMaxLength(500)
+                        .HasColumnType("nvarchar(500)");
+
                     b.Property<string>("DescriptionEng")
                         .IsRequired()
                         .HasMaxLength(2000)
